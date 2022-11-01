db311b5e55a1569ad3f12a5b357436246b17fc9b
muharrem gem <muharremgem28@gmail.com>
2022-11-01 19:39:12 +0300

    cmmt

--- recipe/src/components/pages/Navbar.jsx
+++ recipe/src/components/pages/Navbar.jsx
@@ -3,15 +3,19 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <div className="flex flex-col bg-orange-500 p-7 ">
+      <div className="flex flex-col bg-orange-500 text-white p-5 ">
         <h1>Food House </h1>
+
+        <div className="flex flex-col justify-end items-end">
+          <NavLink to="/home" className="text-green-500" 
+          style={({ isActive}) => ({
+            color
+          })}
+          >
+            Home
+          </NavLink>
+        </div>
       </div>
-
-
-        <NavLink to="/" className="text-green-500">
-          Home{" "}
-        </NavLink>
-
     </>
   );
 };
