3cbc4d7dea3450ba7a03e0b893e14c9f0befd794
muharrem gem <muharremgem28@gmail.com>
2022-11-01 17:27:11 +0300

    commit

---
+++ recipe/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App bg-black">
+      <div className="text-orange-500 bg-black text-bold flex justify-center">
+        {" "}
+        muharrem{" "}
+      </div>
+    </div>
+  );
+}
+
+export default App;
