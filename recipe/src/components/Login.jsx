4e045ffd1704a1e618a8cd5c0d28557f5c0323e0
muharrem gem <muharremgem28@gmail.com>
2022-11-01 18:25:51 +0300

    commit

--- recipe/src/components/Login.jsx
+++ recipe/src/components/Login.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import foto from "../assets/offer.png";
 
 const Login = () => {
   return (
     <div>
-      <div>
-        <h1>Food House</h1>
-
+      <div className="text-2xl flex  flex-col justify-center items-center bg-green-500">
+        <h1 className="font-bold text-3xl ">Food House</h1>
         <h2> Login to See All of the Delicious Recipes</h2>
+      </div>
+      <div>
+        <form>
+          <img className="w-full  " src={foto} alt="foto" />
+          <input type="text" placeholder="Enter your name" />
+        </form>
       </div>
     </div>
   );
