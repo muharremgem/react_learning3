8b90bdab8e1e389ca35047a71387aa46f8f1eeb7
muharrem gem <muharremgem28@gmail.com>
2022-11-01 19:17:02 +0300

    commit

--- recipe/src/components/Login.jsx
+++ recipe/src/components/Login.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import foto from "../assets/offer.png";
+import ascı from "../assets/ascı.jpg";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <div className="text-2xl flex  flex-col justify-center items-center bg-green-500">
@@ -9,9 +12,29 @@ const Login = () => {
         <h2> Login to See All of the Delicious Recipes</h2>
       </div>
       <div>
-        <form>
-          <img className="w-full  " src={foto} alt="foto" />
-          <input type="text" placeholder="Enter your name" />
+        <form onSubmit={() => navigate("Home")}>
+          <div className="foto flex flex-col justify-center items-center">
+            <img className="w-[9rem] bg-transparent       " src={ascı} alt="" />
+            <input
+              className="p-4 m-5 rounded-xl  text-xl"
+              type="text"
+              placeholder="Enter your name"
+              required
+            />
+            <input
+              className="p-4 m-3 rounded-xl text-xl"
+              type="password"
+              placeholder="password"
+              required
+            />
+
+            <button
+              type="submit"
+              className="bg-orange-500 p-3 m-3 px-7 rounded-2xl hover:scale-90 hover:bg-green-500"
+            >
+              Login
+            </button>
+          </div>
         </form>
       </div>
     </div>
